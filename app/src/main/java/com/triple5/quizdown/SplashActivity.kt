5fc8ab62135e636db9f6864a3e53dd3a27f18aa8
Änubhab <hota000anubhab@gmail.com>
2021-01-18 22:02:16 +0530

    Added Splash Screen and removed action bar

---
+++ app/src/main/java/com/triple5/quizdown/SplashActivity.kt
@@ -0,0 +1,40 @@
+package com.triple5.quizdown
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
+
+class SplashActivty : AppCompatActivity() {
+    object Staticated {
+        var SPLASH_TIME_OUT = 2000
+    }
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+            Handler().postDelayed({
+                val startAct = Intent(this@SplashActivty, MainActivity::class.java)
+                startActivity(startAct)
+                // close this activity
+                this.finish()
+            }, SplashActivty.Staticated.SPLASH_TIME_OUT.toLong())
+        }
+
+
+    }
+
+
+
+
+
